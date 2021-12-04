ad9a54ce7a72ba7d605df46d7dc4ac78a661d2ab
Jana Fisdel <janafisdel@gmail.com>
2021-12-04 17:29:29 -0300

    modificacion funcion en contadores

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -7,24 +7,16 @@ function ItemCount({stock, initial}) {
 
     const handleSuma = (valor)=>{
         value < stock ? setValue(previo => previo+ valor): console.log('compra maxima posible')
-
-        // if (value <stock)
-        // setValue(value + 1)
     }
 
     const handleResta =(valor)=>{
         value > initial ? setValue(estadoPrevio => estadoPrevio - valor) : console.log('compra minima posible')
-
-
-
-        // if (value > initial){
-        // setValue(value - 1)
-        // }
-        
     }
-    
-    const handleAgregar =()=>{
-        console.log('Se agregaron' + {} + 'productos al carrito')
+
+
+    //ESTA ES LA CONSULTA QUE TENGO SOBRE LA FUNCION onADD
+    const onAdd =()=>{
+        alert("Producto agregado")
     }
 
     return (
@@ -32,7 +24,7 @@ function ItemCount({stock, initial}) {
         <p>{value}</p>    
         <button onClick={()=>handleSuma(1)}>+</button>
         <button onClick={()=>handleResta(1)}>-</button>
-        <button onClick={handleAgregar}>Agregar al carrito</button>
+        <button onClick={onAdd}>Agregar al carrito</button>
         
         </div>
     )
